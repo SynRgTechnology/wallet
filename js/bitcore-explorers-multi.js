99cd039029cfd9b17a94e44036ebfc182d835045
shannon code <shannoncode@shannons-MacBook-Pro.local>
2016-11-29 13:52:48 -0500

    moved api

--- js/bitcore-explorers-multi.js
+++ js/bitcore-explorers-multi.js
@@ -336,7 +336,7 @@ function Insight(url, network) {
                 url = 'http://wallet.ribbit.me/api/bitcoin'
                 break;
             case "coval":
-                url = "http://ribbitchain.info/"
+                url = "http://coval-legacy-api.mybluemix.net"
                 //url = 'http://wallet.ribbit.me/api/ribbit'
                 break;
             case "test-ribbit":
